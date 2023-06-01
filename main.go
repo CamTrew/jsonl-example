8da1ecae0d6ff2216e03b23ac257415e89ecca5b
Cameron Trew <33068113+camerontrew@users.noreply.github.com>
2023-06-01 21:20:15 +0100

    Initial Commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+// Person is the struct of our JSONL Objects
+type Person struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Address string `json:"address"`
+}
+
+func readJSONLFile(file io.Reader) ([]Person, error) {
+	var people []Person
+
+	decoder := json.NewDecoder(file)
+	// Each iteration will be the next JSON Object
+	for decoder.More() {
+		var person Person
+
+		if err := decoder.Decode(&person); err != nil {
+			return nil, err
+		}
+
+		people = append(people, person)
+	}
+
+	return people, nil
+}
+
+func main() {
+	file, err := os.Open("people.jsonl")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// We can pass in any file which implements io.Reader e.g. multipart.File or os.File
+	people, err := readJSONLFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, person := range people {
+		fmt.Println(person.Name, person.Age, person.Address)
+	}
+}
